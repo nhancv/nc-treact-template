d9c59a5a51ae9aa6b730f24a81c301792e726ed9
Nhan Cao <nhancv92@gmail.com>
2018-10-12 11:06:05 +0700

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,28 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Nhan Cao
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
@@ -6,8 +31,9 @@ import cookieParser from 'cookie-parser';
 import htmlMetaTags from 'html-meta-tags';
 import compression from 'compression';
 import * as fs from "fs";
+import * as ENV from "./env";
 
-const config = require('./env.js').get(process.env.NODE_ENV);
+const config = ENV.get();
 const app = express();
 const log = console.log;
 const port = process.env.PORT || 4000;
